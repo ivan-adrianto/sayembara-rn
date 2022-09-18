5cb07112dbe85b567ca4ab52fa98b16c8ae29d90
Ivan Adrianto <ivanadrianto64@gmail.com>
2022-09-18 09:52:22 +0700

    layouting submit work screen

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,5 +2,6 @@ import IconHome from './icon-home.svg';
 import IconProfile from './icon-profile.svg';
 import IconMenu from './icon-menu.svg';
 import IconArrowDown from './icon-arrow-down.svg';
+import IconPlus from "./icon-plus.svg"
 
-export {IconHome, IconProfile, IconMenu, IconArrowDown};
+export {IconHome, IconProfile, IconMenu, IconArrowDown, IconPlus};
--- src/pages/SubmitWork.js
+++ src/pages/SubmitWork.js
@@ -1,14 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, TextInput, View} from 'react-native';
+import Text from '../components/Text';
+import React from 'react';
+import {IconPlus} from '../assets/icons';
+import {LoginImage} from '../assets/images';
+import Button from '../components/Button';
 
 const SubmitWork = () => {
   return (
-    <View>
-      <Text>SubmitWork</Text>
+    <View style={styles.page}>
+      <View style={styles.titleContainer}>
+        <Text fontSize={28} bold>
+          Upload Your Files
+        </Text>
+      </View>
+      <View style={styles.imageUploadContainer}>
+        <Image source={LoginImage} style={styles.uploadedImage} />
+        <View style={styles.uploadIconContainer}>
+          <IconPlus style={styles.iconPlus} />
+        </View>
+      </View>
+      <View style={styles.descriptionContainer}>
+        <Text fontSize={16} bold>
+          Description
+        </Text>
+        <TextInput
+          multiline={true}
+          numberOfLines={4}
+          style={styles.descriptionBox}
+        />
+        <Button>Submit</Button>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default SubmitWork
+export default SubmitWork;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: 'white',
+    flex: 1,
+    alignItems: "center"
+  },
+  titleContainer: {
+    textAlign: 'center',
+    marginTop: 55,
+    marginBottom: 46,
+  },
+  imageUploadContainer: {
+    flexDirection: 'row',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    paddingVertical: 9,
+    width: 314,
+    marginBottom: 22
+  },
+  uploadedImage: {
+    height: 92,
+    width: 92,
+    marginLeft: 9,
+  },
+  uploadIconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 92,
+    width: 92,
+    backgroundColor: 'rgba(0, 0, 0, 0.25)',
+    borderWidth: 2,
+    borderColor: 'black',
+    marginLeft: 9,
+  },
+  descriptionBox: {
+    height: 108,
+    width: 314,
+    borderRadius: 3,
+    borderWidth: 1,
+    borderColor: "#C0C0C0",
+    marginTop: 7,
+    marginBottom: 23
+  }
+});
